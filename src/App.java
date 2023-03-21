65c4d9ab644fdd0fe67ae79965a9c36c8f161d88
mmparadinha <matheusmparadinha@gmail.com>
2023-03-21 16:52:40 -0300

    feat: criar piramide de números

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Escolha a altura da sua pirâmide: ");
+
+    try {
+      int x = scan.nextInt();
+      for (int i = 1; i <= x; i++) {
+        for (int j = 1; j <= i ; j++) {
+          System.out.print(i);
+        }
+        System.out.print("\n");
+      }
+    } catch (Exception e) {
+      System.out.println("Deve ser um número inteiro");
+    }
+    scan.close();
   }
 }
